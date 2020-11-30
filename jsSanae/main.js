5ce77ae92b3d81baec929a9c6036cc0b6a2ff65c
sanuss <sanussuser@gmail.com>
2020-11-30 15:56:29 +0100

    modification

--- jsSanae/main.js
+++ jsSanae/main.js
@@ -8,13 +8,13 @@ let max=null;
 let min=null;
 let somme=null;
 let moyenne=null;
-
 saisie=prompt('veuillez entrer un nombre : ');
-    saisieInt=parseInt(saisie);
-        console.log(saisieInt);
-        if(saisie!=0){
+saisieInt=parseInt(saisie);
+console.log(saisieInt);
+        
+if(saisie!=0){
     while(saisie!=0) {  
-        total=total+1;
+       
         numbers[total]=saisieInt;
         numbers.push(saisieInt);
         somme = somme+saisieInt;
@@ -22,21 +22,26 @@ saisie=prompt('veuillez entrer un nombre : ');
     saisie=prompt('veuillez entrer un nombre : ');
     saisieInt=parseInt(saisie);
         console.log(saisieInt);
-    }  ;
-    max=numbers[1];
-    min=numbers[1];
-    for(let i=0;i<=numbers.length-1;i++) {
+    }
+    max=numbers[0];
+  
+    for(let i=1;i<=numbers.length;i++) {
         if(max<numbers[i]){
             max = numbers[i];
         }
+    }
+    
+    min=numbers[0];
+    for(let i=1;i<=numbers.length;i++) {
+
         if(min>numbers[i]) {
             min = numbers[i];
         }  
       
-     
     }
-    
    
+    
+
 console.log(total,' nombres saisis \n','Compris entre  ', min, ' et  ', max, '\n Pour une somme de  ', somme, '\n et une moyenne de  ', moyenne);
 alert(total+' nombres saisis \n'+'Compris entre  '+ min+ ' et  '+ max+ '\n Pour une somme de  '+ somme+'\n et une moyenne de  '+ moyenne)
 }
