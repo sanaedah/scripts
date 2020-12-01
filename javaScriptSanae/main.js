acc96cc985377a2872288f86d10b2edb9eda2f51
sanuss <sanussuser@gmail.com>
2020-12-01 01:40:15 +0100

    nouveau javascript

--- javaScriptSanae/main.js
+++ javaScriptSanae/main.js
@@ -1,51 +1,50 @@
 
-let numbers=[];
-let total =null;
-let saisie=null;
-let saisieInt=null;
-let i=null;
-let max=null;
-let min=null;
-let somme=null;
-let moyenne=null;
+let numbers = [];
+let total = null;
+let saisie = null;
+let saisieInt = null;
+let i = null;
+let max = null;
+let min = null;
+let somme = null;
+let moyenne = null;
 
-saisie=prompt('veuillez entrer un nombre : ');
-saisieInt=parseInt(saisie);
+saisie = prompt('veuillez entrer un nombre : ');
+saisieInt = parseInt(saisie);
 console.log(saisieInt);
 
-if(saisie!=0){
-    while(saisie!=0) { 
+if(saisie != 0){
+    while(saisie != 0) { 
         if(!saisieInt){
             saisie = prompt('veuillez entrer un nombre : ');
-            saisieInt=parseInt(saisie);
+            saisieInt = parseInt(saisie);
             console.log(saisieInt);
            
         }else {
             
             numbers.push(saisieInt);
-            somme = somme+saisieInt;
+            somme = somme + saisieInt;
             saisie = prompt('veuillez entrer un nombre : ');
-            saisieInt=parseInt(saisie);
+            saisieInt = parseInt(saisie);
             console.log(saisieInt);
             total++;
-
         }
     }
      
-    max=numbers[0];
-    min=numbers[0];
+    max = numbers[0];
+    min = numbers[0];
 
-    for(let i=1;i<=numbers.length-1;i++) {
-        if(max<numbers[i]){
+    for(let i = 1 ; i <= numbers.length - 1 ; i++) {
+        if(max < numbers[i]){
             max = numbers[i];
         }
 
-        if(min>numbers[i]) {
+        if(min > numbers[i]) {
             min = numbers[i];
         }
     }
 
-    moyenne= somme/total;
+    moyenne = somme / total;
    
     console.log(total,' nombres saisis \n','Compris entre  ', min, ' et  ', max, '\n Pour une somme de  ', somme, '\n et une moyenne de  ', moyenne);
 
